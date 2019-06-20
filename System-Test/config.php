5ad60aa372cf3656a94eff0632f32db116d92cd4
Sanath <sanathk@atconline.biz>
2019-06-20 16:50:34 +0530

    yii folder upload

--- System-Test/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('DBHOST','localhost');
-	define('DBUSER','root');
-	define('DBPASS','12345');
-	define('DBNAME','alike_temple');
-	$conn=mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
-	
-?>
